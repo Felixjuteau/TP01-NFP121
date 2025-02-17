5d89a1c6232b0d112293b57d8c084ac1a27e4446
Felixjuteau <felixjuteau@gmail.com>
2025-02-17 10:27:26 +0100

    Modif
    Modif de la methode dernier jours du mois de la class Date

--- Date.java
+++ Date.java
@@ -132,9 +132,18 @@ public class Date {
 
     private static int dernierJourDuMois(int mois, int annee) {
         switch (mois) {
-            case 1, 3, 5, 7, 8, 10, 12:
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
                 return 31;
-            case 4, 6, 9, 11:
+            case 4:
+            case 6:
+            case 9:
+            case 11:
                 return 30;
             case 2:
                 return bissextile(annee) ? 29 : 28;
